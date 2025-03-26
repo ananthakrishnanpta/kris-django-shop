60d79311fe36698aa9e67c44aa9df5ba5c7f9694
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-03-26 10:03:50 +0530

    edits

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -1,6 +1,9 @@
 from django.db.models import *
+from django.utils.text import slugify
 
-# Create your models here.
+def product_image_upload_path(instance, filename):
+    product_name = slugify(instance.product.name)  # Convert product name to a slug
+    return f'products/{product_name}/{filename}'
 
 class Product(Model):
     name = CharField(max_length=255)
@@ -11,6 +14,9 @@ class Product(Model):
         return f"Product : {self.name}"
 
 class ProductImage(Model):
-    img = ImageField(upload_to='products/')
+    img = ImageField(upload_to=product_image_upload_path)
     default = BooleanField(default=False)
     product = ForeignKey(Product, on_delete=CASCADE, null=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
